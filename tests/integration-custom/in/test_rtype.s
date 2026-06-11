3e657971931fbaa2c2e84e0fc7d1136e4d810180
skateboardant <ley27510@gmail.com>
2026-06-11 21:59:55 +0800

    custom test assembly file for R-type instructions

---
+++ tests/integration-custom/in/test_rtype.s
@@ -0,0 +1,27 @@
+addi x1, x0, 15
+addi x2, x0, 20
+addi x3, x0, -10
+addi x4, x0, -5
+
+add x5, x1, x2
+add x6, x1, x3
+add x7, x2, x1
+sub x8, x1, x3
+
+addi x9, x0, 0x0F
+addi x10, x0, 0x33
+
+and x11, x9, x10
+or x12, x9, x10
+xor x13, x9, x10
+
+addi x14, x0, 2
+addi x15, x0, -16
+
+sll x16, x15, x14
+sra x17, x15, x14
+srl x18, x15, x14
+
+slt x19, x4, x1
+slt x20, x1, x4
+
