4d19210b39d235b9224d007bbc0190d754098522
skateboardant <ley27510@gmail.com>
2026-06-11 15:28:25 +0800

    create custom test for I-type instructions

---
+++ tests/integration-custom/in/test_itype.s
@@ -0,0 +1,16 @@
+addi x1, x0, 15
+addi x2, x1, -20
+
+addi x3, x0, 0x0F
+xori x4, x3, 0xFF
+andi x5, x4, 0xF0
+ori x6, x5, 0x0F
+
+slti x7, x2, 0
+
+addi x9, x0, -16
+
+slli x10, x9, 2
+srai x11, x9, 2
+srli x12, x9, 2
+
